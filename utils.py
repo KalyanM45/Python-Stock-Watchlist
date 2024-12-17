a7b7e13672ba34efa75f942dd7ba1465b6eb0425
KalyanM45 <mhemakalyan@gmail.com>
2024-12-17 09:14:59 +0530

    Project Front End Update

--- utils.py
+++ utils.py
@@ -14,6 +14,13 @@ def load_watchlist():
         return tickers
     except FileNotFoundError:
         return []
+    
+def get_day_suffix(day):
+    if 11 <= day <= 13:
+        suffix = 'th'
+    else:
+        suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(day % 10, 'th')
+    return suffix
 
 def fetch_watchlist(ticker):
     try:
@@ -31,17 +38,27 @@ def fetch_watchlist(ticker):
             
             change = price - prev_close
             change_percent = (change / prev_close) * 100 if prev_close else 0
-            date_time = row.name.strftime('%Y-%m-%d %H:%M:%S')
+
+            # Format the date to "16th Dec 24 12:00 AM"
+            date_time_obj = row.name
+            day = date_time_obj.day
+            suffix = get_day_suffix(day)
+            date_time = date_time_obj.strftime(f"{day}{suffix} %b'%y %I:%M %p")
+
+            # Get the company name
+            company_name = stock.info.get('longName', 'N/A')
+
 
             response =  {
                 'ticker': ticker,
+                'company_name': company_name,
                 'price': f'${price:.2f}',
                 'date_time': date_time,
-                'high': f'{high:.4f}',
-                'low': f'{low:.4f}',
-                'volume': f'{volume:.4f}',
-                'change': f'{change:.4f}',
-                'change_percent': f'{change_percent:.4f}%'
+                'high': f'{high:.2f}',
+                'low': f'{low:.2f}',
+                'volume': f'{volume:.2f}',
+                'change': f'{change:.2f}',
+                'change_percent': f'{change_percent:.2f}%'
             }
 
             return response
